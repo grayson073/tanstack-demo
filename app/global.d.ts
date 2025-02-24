289c7f40691a55ba3af21c2417b99caa262fcd45
grayson073 <k.grayson.ms@gmail.com>
2025-02-23 23:40:44 -0700

    Update declarations

--- app/global.d.ts
+++ app/global.d.ts
@@ -3,3 +3,11 @@ declare module '*.jpeg' {
   const value: string
   export default value
 }
+
+interface ImportMetaEnv {
+  VITE_IMGUR_CLIENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
